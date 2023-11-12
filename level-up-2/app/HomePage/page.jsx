93c1a0b94c3658918ead73f7c4bceba2cb59a494
Giacomo <impoco126@gmail.com>
2023-11-12 14:59:39 +0100

    added styling to progression bar

--- level-up-2/app/HomePage/page.jsx
+++ level-up-2/app/HomePage/page.jsx
@@ -1,36 +1,25 @@
-
-//TODO add styling for progression bar
-
 'use client'
 
-import React, { useState , useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskCard from 'components/TaskCard';
 import ProgressionBar from 'components/ProgressionBar';
 
-
 export default function Home() {
-
-  const tasksFromLS = JSON.parse(localStorage.getItem('tasks')|| '[]');
-  
-  const [tasks, setTasks] = useState([ ...tasksFromLS]);
+  const tasksFromLS = JSON.parse(localStorage.getItem('tasks') || '[]');
+  const [tasks, setTasks] = useState([...tasksFromLS]);
 
   // Update Local Storage whenever tasks change
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }, [tasks]);
 
-  // useEffect(() => {console.log('Task has been changed', tasks);}, [tasks])
-
   const fetchTask = async () => {
     try {
       const response = await fetch('http://localhost:3000/api/tasks');
       const data = await response.json();
-      console.log('data', data);
       if (data && data.task) {
         const newTasks = data.task.filter(nt => !tasks.some(t => t._id === nt._id));
-
-        setTasks(currentTasks => [...currentTasks, ...newTasks]) 
-        console.log('tasks', data.task);
+        setTasks(currentTasks => [...currentTasks, ...newTasks]);
       }
     } catch (error) {
       console.error('Error fetching tasks:', error);
@@ -38,10 +27,22 @@ export default function Home() {
   };
 
   return (
-    <main>
+    <main className="pt-14 pb-16"> {/* Added padding to top and bottom */}
+    <div className="pt-14" >
       <ProgressionBar totalTasks={tasks.length} completedTasks={tasks.filter(task => task.completed).length} />
-      <button onClick={fetchTask}>+ Add Task</button>
-      <TaskCard tasks={tasks} setTasks={setTasks} />
+      </div>
+      <div className="container mx-auto px-4">
+        <TaskCard tasks={tasks} setTasks={setTasks} />
+      </div>
+
+      <div className="fixed bottom-4 left-4">
+  <button onClick={fetchTask} className="bg-yellow-500 text-gray-800 text-white p-4 rounded-full shadow-lg">
+    <svg className="h-6 w-6" fill="black" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
+    </svg>
+  </button>
+</div>
+
     </main>
   );
 }
--- level-up-2/components/ProgressionBar.jsx
+++ level-up-2/components/ProgressionBar.jsx
@@ -1,9 +1,12 @@
 export default function ProgressionBar({ totalTasks, completedTasks }) {
     const completionPercentage = totalTasks > 0 ? (completedTasks / totalTasks) * 100 : 0;
- 
+
     return (
-        <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
-            <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${completionPercentage}%` }}> {completionPercentage.toFixed(0)}%</div>
+        <div className="w-3/4 mx-auto bg-gray-200 rounded-full dark:bg-gray-700 overflow-hidden">
+            <div className="bg-yellow-500 text-gray-800 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full transition-all duration-700 ease-in-out" 
+                 style={{ width: `${completionPercentage}%` }}>
+                {completionPercentage.toFixed(0)}%
+            </div>
         </div>
     );
 }
